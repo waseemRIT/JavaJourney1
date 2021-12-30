4449a8ebbfe4d5549a02f75edce57329d3b9fb39
waseemRIT <whq8052@g.rit.edu>
2021-12-30 17:22:30 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // println has a new line at the end print doesn't end with \n
+        System.out.println("\"HELLO HUMANS\"");
+        System.out.println("lol");
+    }
+}
