ef77f18c246ee13658845b7eef19c079b002fba9
waseemRIT <whq8052@g.rit.edu>
2021-12-30 17:27:23 +0400

    Comments in Java

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         // println has a new line at the end print doesn't end with \n
         System.out.println("\"HELLO HUMANS\"");
         System.out.println("lol");
+
+        // this is a single line comment in Java LoL
+        /*
+        This is a multi line comment in Java LoL
+         */
     }
 }
