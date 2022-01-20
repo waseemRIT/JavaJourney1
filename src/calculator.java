28ee6406f25d2c2e678db52c46519b289d111227
waseemRIT <whq8052@g.rit.edu>
2022-01-20 20:00:17 +0400

    Functionalities Written

--- src/calculator.java
+++ src/calculator.java
@@ -1,8 +1,17 @@
 public class calculator{
-    public static void main(){
+    public static void main(String[] args){
 
-        // Trying the Addition Functions
+        // Trying the Addition Function
         System.out.println(addition(5, 6));
+
+        // Trying the Subtraction Function
+        System.out.println(subtraction(6, 9));
+
+        // Trying the Multiplication Function
+        System.out.println(multiplicaion(9, 9));
+
+        // Trying the Division Fubnction
+        System.out.println(division(9, 5));
 
     }
 
@@ -18,7 +27,7 @@ public class calculator{
 
     // The Division Function
     public static double division(int num1, int num2){
-        return num1 / num2;
+        return (double) num1 / num2;
     }
 
     // The Multiplication Function
