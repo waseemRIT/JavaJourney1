26b1cac3e1b5f9b0adbe67433149efe4cdc92c3c
waseemRIT <whq8052@g.rit.edu>
2022-01-20 19:56:04 +0400

    Calculator Functionalities

--- src/calculator.java
+++ src/calculator.java
@@ -1,5 +1,30 @@
-public class calculator {
+public class calculator{
     public static void main(){
 
+        // Trying the Addition Functions
+        System.out.println(addition(5, 6));
+
     }
+
+    // The Addition Function
+    public static int addition(int num1, int num2){
+        return num1 + num2;
+    }
+
+    // The Subtraction Function
+    public static int subtraction(int num1, int num2){
+        return num1 - num2;
+    }
+
+    // The Division Function
+    public static double division(int num1, int num2){
+        return num1 / num2;
+    }
+
+    // The Multiplication Function
+    public static int multiplicaion(int num1, int num2){
+        return  num1 * num2;
+    }
+    
+
 }
